1731818c25c0919681505074107432d338cb2645
Joe Li <joe.li@okchem.com>
2018-11-16 16:12:05 +0800

    remove layout; add page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/:page', function(req, res, next) {
+  res.render(req.params.page);
 });
 
 module.exports = router;
