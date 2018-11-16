0c9b3b9534cebe286d9bb06081c1bab2e082d251
Joe Li <joe.li@okchem.com>
2018-11-16 15:47:56 +0800

    Add express-favicon

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const favicon = require('express-favicon');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
