4f3ecfc6f88e76c151122ed3e32511729824a99a
Joe Li <joe.lea@foxmail.com>
2018-11-16 23:37:41 +0800

    Refine pages

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index');
 });
 
-router.get('/:page', function(req, res, next) {
-  res.render(req.params.page);
+router.get('/(:page).html', function (req, res, next) {
+  res.render('seo/'+req.params.page);
 });
 
 module.exports = router;
